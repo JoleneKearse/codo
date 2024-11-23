2f9d34fdc3617b98685fce0a4c38c33b9e53b1a5
JoleneKearse <pharaohnutz@gmail.com>
2024-11-23 12:09:43 -0500

    test: reconfigure setup

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";  
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  test: {
-    environment: "happy-dom",
-    globals: true,
-    setupFiles: "./vitest.setup.js",
-  }
-})
+	test: {
+		environment: "happy-dom",
+		globals: true,
+		setupFiles: ["@testing-library/jest-dom/vitest"],
+	},
+});
